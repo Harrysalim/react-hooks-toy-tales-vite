797db6002030140c6c267bd1bf0550a727201168
Harry Salim <salimharry96@gmail.com>
2026-08-14 11:29:19 +0300

    Implemented Toylist display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+/*import { aa } from "vitest/dist/chunks/reporters.6vxQttCV.js";*/
+import ToyCard from "./ToyCard";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
 
+  const [toyList, setToyList] = useState([]);
+
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+
+  async function addToy(formData) {}
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToyList(data));
+  }, []);
 
   return (
     <>
@@ -19,6 +31,10 @@ function App() {
         <button onClick={handleClick}>Add a Toy</button>
       </div>
       <ToyContainer />
+
+      {toyList.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))}
     </>
   );
 }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToyForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+  });
+
+  function HandleChange(e) {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function HandleSubmit(e) {
+    e.preventDefault();
+    setFormData(formData);
+
+    setFormData({
+      name: "",
+      image: "",
+    });
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={HandleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          value={formData.name}
+          onChange={HandleChange}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +38,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          value={formData.image}
+          onChange={HandleChange}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
